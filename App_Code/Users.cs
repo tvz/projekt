7ff937bfbe451041bfeb2373301abaf43bc92b70
Emilio <me@server.com>
2013-05-09 14:36:46 +0200

    ubacen test user u bazu username:test pass:test

---
+++ App_Code/Users.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// Klasa je nazvana Users zato što klasa User vec postoji
+/// </summary>
+public class Users
+{
+    public enum databaseColumnName { ID,
+                                     username,
+                                     password_hash,
+                                     pass_salt,
+                                     created_at,
+                                     updated_at,
+                                     role_id,
+                                     paypal_id,
+                                     email,
+                                     status_id };
+    private int id;
+    public string username;
+    public string password_hash;
+    public string pass_salt;
+
+    public Users()
+    {
+    }
+}
