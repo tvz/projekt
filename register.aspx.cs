adee159f832254dbef0f33c064deb721cab7d882
Ivan Pericki <ipericki1@tvz.hr>
2013-05-09 22:08:18 +0200

    dovrsena register forma i metoda

--- register.aspx.cs
+++ register.aspx.cs
@@ -1,22 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
+using System.Security.Cryptography;
+using System.Web.Configuration;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.OleDb;
+using System.Text;
+
 
 public partial class register : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+       
     }
 
     /*developer:Ivan
-     description: metoda kreira korisnika i odmah ga preusmjerava na korisnicke stranice
+     description: metoda kreira korisnika i odmah ga preusmjerava na index.aspx
+     notice: kod passworda trazi non-alphanumeric znak, trenutno nije rijesen problem;
+     security question i answer su skriveni, a po defaultu su zadani (radi validatora)
+     ako otkrijete neki neocekivani problem, molim da se javi
      */
+
     protected void CreateUserWizard1_CreatedUser(object sender, EventArgs e)
     {
+        OleDbConnection conn = new OleDbConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString);
+        OleDbCommand command = new OleDbCommand();
+        SHA1CryptoServiceProvider sha1Algorithm = new SHA1CryptoServiceProvider();
+        UnicodeEncoding unEncode = new UnicodeEncoding();
+        string pass_salt, prehashed_pass, hashed_pass, created, updated;
 
+        try
+        {
+            pass_salt = FormsAuthentication.HashPasswordForStoringInConfigFile(DateTime.Now.ToString(), "SHA1").ToLower();
+            prehashed_pass = CreateUserWizard1.Password.ToString() + pass_salt;
+            hashed_pass = FormsAuthentication.HashPasswordForStoringInConfigFile(prehashed_pass, "SHA1").ToLower();
+
+            created = DateTime.Now.Date.ToShortDateString();
+            updated = DateTime.Now.Date.ToShortDateString();
+
+            command.CommandText = "INSERT INTO users ([username], [password_hash], [pass_salt], [email], [created_at], [updated_at]) VALUES (@username, @password_hash, @pass_salt, @email, @created_at, @updated_at)";
+            command.Parameters.AddWithValue("@username", CreateUserWizard1.UserName.ToString());
+            command.Parameters.AddWithValue("@password_hash", hashed_pass);
+            command.Parameters.AddWithValue("@pass_salt", pass_salt);
+            command.Parameters.AddWithValue("@email", CreateUserWizard1.Email.ToString());
+            command.Parameters.AddWithValue("@created_at", created);
+            command.Parameters.AddWithValue("@updated_at", updated);
+
+            command.Connection = conn;
+            conn.Open();
+            command.ExecuteNonQuery();
+        }
+        catch { }
+        finally
+        {
+            conn.Close();
+        }
     }
 }
