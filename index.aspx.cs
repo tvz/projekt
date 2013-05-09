85dd99bbc05e62aaeab03be8348ba8c92530febf
Andor <andorzg@gmail.com>
2013-05-09 22:37:39 +0200

    test

--- index.aspx.cs
+++ index.aspx.cs
@@ -10,7 +10,7 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
-
+//jer ima promjena test
 public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
