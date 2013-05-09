d299bd1097accc2559c4427ff38df3a8ed3ba4e1
Emilio <me@server.com>
2013-05-09 13:09:13 +0200

    dodana login forma, i prazna Login1_Authenticate metoda

--- login.aspx
+++ login.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="Default2" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="login.aspx.cs" Inherits="_Default" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
@@ -9,6 +9,9 @@
 <body>
     <form id="form1" runat="server">
     <div>
+    
+        <asp:Login ID="Login1" runat="server" onauthenticate="Login1_Authenticate">
+        </asp:Login>
     
     </div>
     </form>
---
+++ login.aspx.cs
@@ -0,0 +1,27 @@
+﻿/*developer: Emilo Brutus*/
+using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    /*developer:Emilio
+     description: metoda logira korisnika i preusmjerava ga na korisnicke stranice
+     */
+    protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+
+    }
+}
