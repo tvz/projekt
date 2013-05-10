dc62a0d47cdbf66f095bbdfa956c8adbc1e124ad
Emilio <me@server.com>
2013-05-10 16:33:30 +0200

    ubacen datum u polje 'Do kraja'

--- index.aspx.cs
+++ index.aspx.cs
@@ -36,7 +36,7 @@ public partial class Default2 : System.Web.UI.Page
             +"<h3><b>AUTOR PROJEKTA:</b> "+project.project_owner_username+ "</h3>"
             +"<h3><b>OPIS PROJEKTA:</b> "+project.description+" </h3>"
             +"<h3><b>SAKUPLJENO:</b> "+project.DonationSum() +" Kunića "+"("+ project.DonationsPercent() +"%)"+"</h3>"
-            +"<h3><b>DO KRAJA:</b> " + (project.goal - project.DonationSum()) + "Kunića"+"</h3>"
+            +"<h3><b>DO KRAJA:</b> " + (project.expiration_date - DateTime.Now).Days + "Dana"+"</h3>"
             +"<input type='submit' name='Button4' value='DONIRAJ!' class='gumb' />"
             +"</div>";
             }
@@ -48,7 +48,7 @@ public partial class Default2 : System.Web.UI.Page
             + "<h3><b>AUTOR PROJEKTA:</b> " + project.project_owner_username + "</h3>"
             + "<h3><b>OPIS PROJEKTA:</b> " + project.description + " </h3>"
             + "<h3><b>SAKUPLJENO:</b> " + project.DonationSum() + " Kunića " + "(" + project.DonationsPercent() + "%)" + "</h3>"
-            + "<h3><b>DO KRAJA:</b> " + (project.goal - project.DonationSum()) + "Kunića" + "</h3>"
+            + "<h3><b>DO KRAJA:</b> " + (project.expiration_date - DateTime.Now).Days + "Dana" + "</h3>"
             + "<input type='submit' name='Button4' value='DONIRAJ!' class='gumb' />"
             + "</div>";
             }
