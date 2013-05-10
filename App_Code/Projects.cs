1b693e424195e7f0c5d330c2029870af987e7cd3
Emilio <me@server.com>
2013-05-10 15:23:53 +0200

    zavrsena metoda koja lista projekte na index.aspx

---
+++ App_Code/Projects.cs
@@ -0,0 +1,82 @@
+﻿/// <summary>
+/// Klasa Projects predstavlja projects entitet u bazi podataka
+/// </summary>
+/// 
+using System.Data.OleDb;
+using System.Configuration;
+using System.Collections.Generic;
+using System;
+public class Projects
+{
+    /*varijable koje predstavljaju atribute u bazi*/
+    int id;
+    public string name;
+    public string description;
+    public float goal;
+    public string image_path;
+
+    /*sve ostale varijable*/
+    private static OleDbConnection conn = new OleDbConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString);
+    private static OleDbCommand command = new OleDbCommand();
+    private static OleDbDataReader reader;
+    public string project_owner_username;
+
+    public Projects()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    /*developer: Emilio
+     descripion: metoda dohvaca sve projekte i vraca listu projekata*/
+    public static List<Projects> fetch_all()
+    {
+        command.Connection = conn;
+        //command.CommandText = "SELECT ID,name,description,goal FROM projects";
+        command.CommandText = "SELECT projects.ID,projects.name, projects.description, projects.goal,projects.image_path, users.username FROM (projects INNER JOIN users ON projects.user_id = users.ID) GROUP BY projects.ID, projects.name, projects.description, projects.goal, projects.image_path, users.username";
+
+        List<Projects> projects_list = new List<Projects>();
+        try
+        {
+            conn.Open();
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int i = -1;
+                Projects project = new Projects();
+                project.id = Convert.ToInt32(reader.GetValue(++i));
+                project.name = reader.GetValue(++i).ToString();
+                project.description = reader.GetValue(++i).ToString();
+                project.goal = Convert.ToSingle(reader.GetValue(++i));
+                project.image_path = reader.GetValue(++i).ToString();
+                project.project_owner_username = reader.GetValue(++i).ToString();
+                
+                projects_list.Add(project);
+            }
+
+        }
+        catch
+        {
+        }
+        finally
+        {
+            //command.Dispose();
+            //reader.Close();
+            conn.Close();
+        }
+        return projects_list;
+    }
+    /*developer: Emilio
+     descripion: metoda vraca sumu sonacija za projekt*/
+    public float DonationSum()
+    {
+        return Transactions.PaymentGrossSum(this.id);
+    }
+    /*developer: Emilio
+     descripion: metoda vraca postotak sakupljenih donacija u odnosu na project goal*/
+    public float DonationsPercent()
+    {
+        return (Transactions.PaymentGrossSum(this.id) / this.goal) * 100;
+    }
+}
--- App_Code/Users.cs
+++ App_Code/Users.cs
@@ -1,24 +1,71 @@
 ﻿/// <summary>
-/// Klasa je nazvana Users zato što klasa User vec postoji
+///
+/// Klasa Users predstavlja users entitet u bazi podataka
 /// </summary>
+using System.Data.OleDb;
+using System.Configuration;
+using System.Web.Security;
 public class Users
 {
-    public enum databaseColumnName { ID,
-                                     username,
-                                     password_hash,
-                                     pass_salt,
-                                     created_at,
-                                     updated_at,
-                                     role_id,
-                                     paypal_id,
-                                     email,
-                                     status_id };
-    private int id;
-    public string username;
-    public string password_hash;
-    public string pass_salt;
+    
+    /*varijable koje predstavljaju atribute u bazi*/
+    private static int id;
+    private static string username;
+    private static string  password_hash;
+    private static string  pass_salt;
+
+
+
+    /*sve ostale varijable*/
+    private static OleDbConnection conn = new OleDbConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString);
+    private static OleDbCommand command = new OleDbCommand();
+    private static OleDbDataReader reader;
 
     public Users()
     {
+    }
+
+    public static bool login(string username, string password)
+    {
+
+        string prehashed_password;
+        string password_hash2;
+        bool login_status = false;
+
+        command.Connection = conn;
+        command.CommandText = "SELECT password_hash, pass_salt FROM users WHERE username = @username";
+        command.Parameters.AddWithValue("@username", username);
+
+        try
+        {
+            conn.Open();
+            reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+
+                pass_salt = reader.GetValue(1).ToString();
+                password_hash = reader.GetValue(0).ToString();
+                prehashed_password = password + pass_salt;
+                password_hash2 = FormsAuthentication.HashPasswordForStoringInConfigFile(prehashed_password, "SHA1").ToLower();
+                if (password_hash == password_hash2)
+                {
+                    login_status = true;
+                    FormsAuthentication.SetAuthCookie(username, true);
+                    FormsAuthentication.RedirectFromLoginPage(username, true);
+                }
+            }
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            //command.Dispose();
+            command.Parameters.Clear();
+            reader.Close();
+            conn.Close();
+        }
+        return login_status;
     }
 }
