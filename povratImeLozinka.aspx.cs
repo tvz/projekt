c6521aa17b983fb4766780052a44b4e1798a584a
Ivan Pericki <ipericki1@tvz.hr>
2013-05-26 20:42:42 +0200

    manji sigurnosni ispravak

--- povratImeLozinka.aspx.cs
+++ povratImeLozinka.aspx.cs
@@ -16,6 +16,13 @@ public partial class povratImeLozinka : System.Web.UI.Page
 
     }
 
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        Response.Cache.SetExpires(DateTime.UtcNow.AddMinutes(-1));
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        Response.Cache.SetNoStore();
+    }
+
     protected void LinkButtonPassword_Click(object sender, EventArgs e)
     {
         this.MultiViewRetrieve.ActiveViewIndex = 0;
