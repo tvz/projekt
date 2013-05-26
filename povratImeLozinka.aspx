7715a7c39947fef940beb48383b83fabf7ab0fb0
Ivan Pericki <ipericki1@tvz.hr>
2013-05-26 14:53:14 +0200

    napravljen recover password/korisnicko ime

---
+++ povratImeLozinka.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="povratImeLozinka.aspx.cs" Inherits="povratImeLozinka" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>Povrat korisničkog imena ili lozinke</title>
+    <!-- evo malo posla za dizajnere :) -->
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Panel ID="Panel1" runat="server">
+        <asp:LinkButton ID="LinkButtonPassword" runat="server" 
+            onclick="LinkButtonPassword_Click">Povrat lozinke</asp:LinkButton>
+        <br />
+        <asp:LinkButton ID="LinkButtonUsername" runat="server" 
+            onclick="LinkButtonUsername_Click">Povrat korisničkog imena</asp:LinkButton>
+        <br />
+        <hr />
+        <asp:MultiView ID="MultiViewRetrieve" runat="server">
+            <asp:View ID="ViewPassword" runat="server">
+                <asp:Label ID="LabelEmailPassword" runat="server" Text="E-mail: "></asp:Label>
+                <asp:TextBox ID="TextBoxEmailPassword" runat="server"></asp:TextBox>
+                <br />
+                <asp:Label ID="LabelQuestion" runat="server" Text="Sigurnosno pitanje:" 
+                    Visible="False"></asp:Label>
+                <asp:TextBox ID="TextBoxQuestion" runat="server" Visible="False" 
+                    Enabled="False"></asp:TextBox>
+                <br />
+                <asp:Label ID="LabelAnswer" runat="server" Text="Sigurnosni odgovor:" 
+                    Visible="False"></asp:Label>
+                <asp:TextBox ID="TextBoxAnswer" runat="server" Visible="False"></asp:TextBox>
+                <br />
+                <asp:Button ID="ButtonSubmitPassword" runat="server" Text="Pošalji" 
+                    onclick="ButtonSubmitPassword_Click" />
+            </asp:View>
+            <asp:View ID="ViewUsername" runat="server">
+                <asp:Label ID="LabelEmail" runat="server" Text="E-mail:"></asp:Label>
+                <asp:TextBox ID="TextBoxEmail" runat="server"></asp:TextBox>
+                <asp:Button ID="ButtonSubmitUsername" runat="server" Text="Pošalji" 
+                    onclick="ButtonSubmitUsername_Click" />
+                <br />
+            </asp:View>
+        </asp:MultiView>
+        </asp:Panel> 
+    </div>
+    <p>
+         <asp:Label ID="LabelStatus" runat="server" Text=""></asp:Label>
+    </p>
+    </form>
+</body>
+</html>
---
+++ povratImeLozinka.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+using System.Configuration;
+using System.Timers;
+
+public partial class povratImeLozinka : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void LinkButtonPassword_Click(object sender, EventArgs e)
+    {
+        this.MultiViewRetrieve.ActiveViewIndex = 0;
+    }
+
+    protected void LinkButtonUsername_Click(object sender, EventArgs e)
+    {
+        this.MultiViewRetrieve.ActiveViewIndex = 1;
+    }
+
+    /*
+     * developer: Ivan
+     * description: metoda prima te šalje argumente i javlja korisniku status
+     * u vezi s procesom vracanja lozinke
+     */
+    protected void ButtonSubmitPassword_Click(object sender, EventArgs e)
+    {
+        string email, question;
+        email = this.TextBoxEmailPassword.Text;
+        question = this.TextBoxQuestion.Text;
+
+        if (TextBoxQuestion.Text.Length == 0)
+        {
+            question = Users.checkAndGeneratePassword(null, email, null);
+            if (question != null)
+            {
+                this.TextBoxQuestion.Text = question;
+                this.TextBoxQuestion.Visible = true;
+                this.LabelQuestion.Visible = true;
+                this.LabelAnswer.Visible = true;
+                this.TextBoxAnswer.Visible = true;
+                this.TextBoxEmailPassword.Enabled = false;
+            }
+            else this.LabelStatus.Text = "Korisnik s upisanom e-mail adresom ne postoji.";
+        }
+        else if (TextBoxQuestion.Text.Length > 0)
+        {
+            string answer = Users.checkAndGeneratePassword(null, email, this.TextBoxAnswer.Text);
+            if (answer != null)
+            {
+                Panel1.Controls.Clear();
+                this.LabelStatus.Text = "Provjerite e-mail pretinac.";
+            }
+            else
+                this.LabelStatus.Text = "Unesena kombinacija sigurnosnog pitanja i odgovora ne postoji.";
+        }
+
+    }
+
+    /*developer: Ivan
+     */
+    protected void ButtonSubmitUsername_Click(object sender, EventArgs e)
+    {
+        bool status = Users.checkAndGiveUsername(this.TextBoxEmail.Text);
+        if (status == true)
+        {
+            Panel1.Controls.Clear();
+            this.LabelStatus.Text = "Provjerite e-mail pretinac.";
+        }
+        else
+            this.LabelStatus.Text = "Korisnik s upisanom e-mail adresom ne postoji.";
+    }
+
+}
