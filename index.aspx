c32ecc860a8c9e8b919f496e7c8b2020579e30a6
Ivan Pericki <ipericki1@tvz.hr>
2013-06-01 00:16:06 +0200

    dodana funkcija listanja projekata na indexu (3 projekta max)

--- index.aspx
+++ index.aspx
@@ -3,25 +3,38 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
-
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
     <form id="Form1" runat="server">
-    <h1>Najnoviji projekti</h1>       
+    <h1 id="naslovNovi" runat="server">Najnoviji projekti</h1>
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+    </asp:ScriptManager>
     
-    <div runat="server" id="projekti_novi" style="height:594px;overflow:auto;">
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">   
+    <ContentTemplate>
+    <asp:Button ID="scrollNewLeft" runat="server" Text="Nazad" onclick="scroll" Visible="False" />
+    <asp:Button ID="scrollNewRight" runat="server" Text="Naprijed" onclick="scroll" />
+    <div runat="server" id="projekti_novi" style="height:594px;">
     </div>
+    </ContentTemplate>
+        <Triggers>
+            <asp:AsyncPostBackTrigger ControlID="scrollNewRight" EventName="Click" />
+            <asp:AsyncPostBackTrigger ControlID="scrollNewLeft" EventName="Click" />
+        </Triggers>
+    </asp:UpdatePanel>
 
     <!-- služi da bi spriječio preklapanje novih i starih projekata -->
     <div style="margin:auto;clear:both;"></div>
-
+    
     <hr class="druga_lin" />
 
-    <h1>Projekti pred istekom vremena za donaciju</h1>
-
-    <div runat="server" id="projekti_stari" style="height:594px;overflow:auto;">    
+    <h1 id="naslovStari" runat="server">Projekti pred istekom vremena za donaciju</h1>
+    <asp:UpdatePanel ID="UpdatePanel2" runat="server">
+    <ContentTemplate>
+    <asp:Button ID="scrollOldLeft" runat="server" Text="Nazad" onclick="scroll" />
+    <asp:Button ID="scrollOldRight" runat="server" Text="Naprijed" onclick="scroll" />
+    <div runat="server" id="projekti_stari" style="height:594px;overflow:auto;">
     </div>
-
-    <a href="userPage.aspx">Administracija mojih projekata</a>
+    </ContentTemplate>
+    </asp:UpdatePanel>
     </form>
-    
 </asp:Content>
--- index.aspx.cs
+++ index.aspx.cs
@@ -16,26 +16,112 @@ using System.Collections.Generic;
 
 public partial class index : System.Web.UI.Page
 {
+    private static int start = 0, end = 3, length;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        ListProjects();
-        Session["list_projects"] = null;
-        
+        ListProjects(start, end);
+        Session["list_projects"] = null;     
+    }
+
+    /*developer:Ivan
+     * description: metoda gleda koji je button pritisnut te salje zahtjev za listanje
+     */
+    protected void scroll(object sender, EventArgs e)
+    {
+        Button bttn = (Button)sender;
+
+        if (bttn.ID == "scrollNewRight")
+        {
+            scrollNewLeft.Visible = true;
+            if (length > (start + 3))
+            {
+                start += 3;
+                if (length > (start + end))
+                    end = 3;
+                else end = length - start - 1;
+            }
+            ListProjects(start, end);
+            if (((length - 1) - start - end) == 0)
+                scrollNewRight.Visible = false;
+        }
+        else if (bttn.ID == "scrollNewLeft")
+        {
+            scrollNewRight.Visible = true;
+            end = 3;
+            if (0 <= (start - 3))
+                start -= 3;
+            ListProjects(start, end);
+            if (start == 0)
+                scrollNewLeft.Visible = false;
+        }
+        else if (bttn.ID == "scrollOldLeft")
+        {
+            //nedovrseno
+        }
+        else if (bttn.ID == "scrollOldRight")
+        {
+            //nedovrseno
+        }
     }
 
     /*developer: Emilio
     description: metoda cita projekte iz baze i prikazuje na index.aspx*/
-    private void ListProjects()
+    //notice:dogovor jer da se ne prcka po tudjim metodama, no buduci da ti delas zavrsni,
+    //netko je moral listanje napravit :)
+    private void ListProjects(int start, int end)
     {
         List<Projects> projects_list = Projects.fetch_all();
+        length = projects_list.Count;
+        List<Projects> newProjects = new List<Projects>();
+        List<Projects> oldProjects = new List<Projects>();
+
+        //privremene liste koje koristimo zbog listanja
+        List<Projects> newProjectsTemp;
+        List<Projects> oldProjectsTemp;
 
         string html = null;
-
         projekti_novi.InnerHtml = "";
         projekti_stari.InnerHtml = "";
 
-
+        //podjela na stare i nove projekte
         foreach (Projects project in projects_list)
+        {
+            if (DateTime.Now.AddDays(7) >= project.expiration_date)
+                oldProjects.Add(project);
+            else newProjects.Add(project);
+        }
+
+        naslovNovi.InnerHtml = "Najnoviji projekti (" + newProjects.Count + ")";
+        naslovStari.InnerHtml = "Projekti pred istekom vremena za donaciju (" + oldProjects.Count + ")";
+
+        //ako se u listi nalazi do 3 projekta
+        if (newProjects.Count <= 3)
+        {
+            scrollNewRight.Visible = false;
+            scrollNewLeft.Visible = false;
+            newProjectsTemp = newProjects;
+        }
+        else
+        {
+            scrollNewRight.Visible = true;
+            newProjectsTemp = newProjects.GetRange(start, end);
+        }
+
+        if (oldProjects.Count <= 3)
+        {
+            scrollOldRight.Visible = false;
+            scrollOldLeft.Visible = false;
+            oldProjectsTemp = oldProjects;
+        }
+        else
+        {
+            scrollOldRight.Visible = true;
+            oldProjectsTemp = oldProjects.GetRange(start, end);
+        }
+
+        //novi projekti
+        foreach (Projects project in newProjectsTemp)
         {
             HtmlButton button = new HtmlButton();
             button.Attributes.Add("class", "gumb");
@@ -45,14 +131,14 @@ public partial class index : System.Web.UI.Page
             if (project.video_path.Length > 0)
             {
                 html = "<h2>" + project.name + "</h2>"
-                    + "&nbsp;&nbsp;&nbsp;<iframe width='320' height='180' src='"+project.video_path+"' frameborder='0' allowfullscreen></iframe>";
+                    + "&nbsp<iframe width='320' height='180' src='" + project.video_path + "' frameborder='0' allowfullscreen></iframe>";
             }
-            else 
+            else
             {
                 html = "<h2>" + project.name + "</h2>"
                     + "<img  src=" + "'" + project.image_path + "'" + " alt=" + "'" + project.name + "'" + "> ";
             }
-            html+="<h3><b>AUTOR PROJEKTA:</b> " + project.project_owner_username + "</h3>"
+            html += "<h3><b>AUTOR PROJEKTA:</b> " + project.project_owner_username + "</h3>"
             + "<h3><b>OPIS PROJEKTA:</b> " + project.description + " </h3>"
             + "<h3><b>SAKUPLJENO:</b> " + project.DonationSum() + " Kunića " + "(" + project.DonationsPercent() + "%)" + "</h3>"
             + "<h3><b>DO KRAJA:</b> " + (project.expiration_date - DateTime.Now).Days + "Dana" + "</h3>";
@@ -61,17 +147,40 @@ public partial class index : System.Web.UI.Page
             div.Attributes.Add("class", "proj");
             div.InnerHtml = html;
             div.Controls.Add(button);
+            projekti_novi.Controls.Add(div);
+        }
 
-            if (DateTime.Now.AddDays(7) >= project.expiration_date)
+        //stari projekti
+        foreach (Projects project in oldProjectsTemp)
+        {
+            HtmlButton button = new HtmlButton();
+            button.Attributes.Add("class", "gumb");
+            button.InnerText = "DONIRAJ";
+            button.ID = project.id.ToString();
+            button.ServerClick += new EventHandler(MakeDonation);
+            if (project.video_path.Length > 0)
             {
-                projekti_stari.Controls.Add(div);
+                html = "<h2>" + project.name + "</h2>"
+                    + "&nbsp<iframe width='320' height='180' src='" + project.video_path + "' frameborder='0' allowfullscreen></iframe>";
             }
-            else//tu ce jos ici provjera za stare projekte. jos nije odredeno kada postaju stari :-)
+            else
             {
-                projekti_novi.Controls.Add(div);
+                html = "<h2>" + project.name + "</h2>"
+                    + "<img  src=" + "'" + project.image_path + "'" + " alt=" + "'" + project.name + "'" + "> ";
             }
+            html += "<h3><b>AUTOR PROJEKTA:</b> " + project.project_owner_username + "</h3>"
+            + "<h3><b>OPIS PROJEKTA:</b> " + project.description + " </h3>"
+            + "<h3><b>SAKUPLJENO:</b> " + project.DonationSum() + " Kunića " + "(" + project.DonationsPercent() + "%)" + "</h3>"
+            + "<h3><b>DO KRAJA:</b> " + (project.expiration_date - DateTime.Now).Days + "Dana" + "</h3>";
+
+            HtmlGenericControl div = new HtmlGenericControl("div");
+            div.Attributes.Add("class", "proj");
+            div.InnerHtml = html;
+            div.Controls.Add(button);
+            projekti_stari.Controls.Add(div);
         }
     }
+
     /*developer: Emilio
      description: metoda salje donaciju preko paypala
      metoda ce se u buducnosti izmijeniti i bolje strukturirati, sad sluzi samo kao test.*/
